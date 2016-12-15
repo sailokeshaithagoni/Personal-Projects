e8e901fc2771808c6c996c03829a2d4a54125b20
sailokeshgoud <sailokeshgoud@gmail.com>
2016-12-15 12:05:40 +0530

    WebViewApps

---
+++ WebViewApps/WebViewApps/WebViewApps/Page.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace WebViewApps
+{
+    public partial class Page : ContentPage
+    {
+        public Page(string URL)
+        {
+            InitializeComponent();
+            Browser.Source = URL;
+        }
+        private void backClicked(object sender, EventArgs e)
+        {
+            // Check to see if there is anywhere to go back to
+            if (Browser.CanGoBack)
+            {
+                Browser.GoBack();
+            }
+        }
+        private void forwardClicked(object sender, EventArgs e)
+        {
+            if (Browser.CanGoForward)
+            {
+                Browser.GoForward();
+            }
+        }
+    }
+}
+
